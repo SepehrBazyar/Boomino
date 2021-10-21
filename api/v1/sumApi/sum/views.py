fba352c38ed9567a0c70b8d10568302881ee8576
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-10-22 00:43:05 +0330

    add limiter on sum route 100 times in a hour

--- api/v1/sumApi/sum/views.py
+++ api/v1/sumApi/sum/views.py
@@ -1,15 +1,18 @@
 from fastapi import (
     APIRouter,
+    Depends,
     HTTPException,
     status
 )
+from fastapi_limiter.depends import RateLimiter
 from .utils import *
 
 router = APIRouter()
 
 @router.get(
     "/sum/",
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(RateLimiter(times=100, hours=1))]
 )
 async def sum_view(
     a: int,
